cc1aa378b3b5911a67b16b2ca77dcf51583c7968
emilrl <emil1708@gmail.com>
2015-08-31 16:09:27 +0200

    Revert "Problem 012 solved"
    This reverts commit 840f5abc9ef85e7b7bf7c94821de19a4ec071c3f.

--- Problem012/Program.cs
+++ Problem012/Program.cs
@@ -8,33 +8,29 @@ namespace Problem012
 {
     public class Program
     {
-        public static int TriangleNumber(int maxDivisorCount)
+        public static long TriangleNumber(long maxDivisorCount)
         {
-            var divisorCount = 0;
-            var i = 1;
-            var j = 2;
+            long divisorCount = 0;
+            long i = 1;
+            long j = 2;
             while (divisorCount < maxDivisorCount)
             {
-                i = i + j;
-                j ++;
-                var divisors = DivisorCounter(i);
+                var divisors = 0;
+                var kMax = i/2;
+                for (var k = 2; k < kMax; k++)
+                {
+                    if (i%k == 0)
+                    {
+                        divisors++;
+                    }
+                }
+                divisors ++;
+                i =+ j;
+                j++;
 
                 divisorCount = divisors;
             }
-            return i;
-        }
-
-        public static int DivisorCounter(int i)
-        {
-            var divisors = 0;
-            for (var k = 1; k <= (Math.Sqrt(i)); k++)
-            {
-                if (i % k == 0)
-                {
-                    divisors += 2;
-                }
-            }
-            return divisors;
+            return j;
         }
     }
 }
--- Tests/001-100/012/Problem012Tests.cs
+++ Tests/001-100/012/Problem012Tests.cs
@@ -7,52 +7,10 @@ namespace Tests._012
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod, TestCategory("Answer")]
-        public void _012TriangleNumberTests5()
-        {
-            var result = Program.TriangleNumber(500);
-            Console.WriteLine(result);
-        }
-
         [TestMethod, TestCategory("Check")]
-        public void _012TriangleNumberTests200()
+        public void _012TriangleNumberTests()
         {
-            var result = Program.TriangleNumber(200);
-            Console.WriteLine(result);
-        }
-
-        [TestMethod, TestCategory("Check")]
-        public void _012DivisorCounterTests6()
-        {
-            var result = Program.DivisorCounter(6);
-            Console.WriteLine(result);
-        }
-
-        [TestMethod, TestCategory("Check")]
-        public void _012DivisorCounterTests10()
-        {
-            var result = Program.DivisorCounter(10);
-            Console.WriteLine(result);
-        }
-
-        [TestMethod, TestCategory("Check")]
-        public void _012DivisorCounterTests15()
-        {
-            var result = Program.DivisorCounter(15);
-            Console.WriteLine(result);
-        }
-
-        [TestMethod, TestCategory("Check")]
-        public void _012DivisorCounterTests28()
-        {
-            var result = Program.DivisorCounter(495);
-            Console.WriteLine(result);
-        }
-
-        [TestMethod, TestCategory("Check")]
-        public void _012TriangleNumberFinderTests()
-        {
-            var result = Program.TriangleNumberFinder(1);
+            var result = Program.TriangleNumber(2);
             Console.WriteLine(result);
         }
     }
