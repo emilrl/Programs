840f5abc9ef85e7b7bf7c94821de19a4ec071c3f
emilrl <emil1708@gmail.com>
2015-08-31 15:28:43 +0200

    Problem 012 solved

--- Tests/001-100/012/Problem012Tests.cs
+++ Tests/001-100/012/Problem012Tests.cs
@@ -7,10 +7,52 @@ namespace Tests._012
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod, TestCategory("Check")]
-        public void _012TriangleNumberTests()
+        [TestMethod, TestCategory("Answer")]
+        public void _012TriangleNumberTests5()
         {
-            var result = Program.TriangleNumber(2);
+            var result = Program.TriangleNumber(500);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod, TestCategory("Check")]
+        public void _012TriangleNumberTests200()
+        {
+            var result = Program.TriangleNumber(200);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod, TestCategory("Check")]
+        public void _012DivisorCounterTests6()
+        {
+            var result = Program.DivisorCounter(6);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod, TestCategory("Check")]
+        public void _012DivisorCounterTests10()
+        {
+            var result = Program.DivisorCounter(10);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod, TestCategory("Check")]
+        public void _012DivisorCounterTests15()
+        {
+            var result = Program.DivisorCounter(15);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod, TestCategory("Check")]
+        public void _012DivisorCounterTests28()
+        {
+            var result = Program.DivisorCounter(495);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod, TestCategory("Check")]
+        public void _012TriangleNumberFinderTests()
+        {
+            var result = Program.TriangleNumberFinder(1);
             Console.WriteLine(result);
         }
     }
