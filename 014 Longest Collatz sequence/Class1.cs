c948e62846bd1fc29648bd937adf584bda725f89
emilrl <emil1708@gmail.com>
2015-09-13 04:21:44 +0200

    Problem 014 started

---
+++ 014 Longest Collatz sequence/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_Longest_Collatz_sequence
+{
+    public class Class1
+    {
+        public static int even(int number)
+        {
+            return number/2;
+        }
+
+        public static int uneven(int number)
+        {
+            return (number*3) + 1;
+        }
+
+        public static int LongestCollatz(int belowNumber)
+        {
+            var collatzList = new int[(belowNumber), 2];
+            var longestNum = 0;
+            var longestLength = 0;
+            for (var i = 0; i < belowNumber; i++)
+            {
+                var j = i;
+                var count = 0;
+                while (j > 1)
+                {
+                    if (j < i)
+                    {
+                        count = count + collatzList[j, 1];
+                        break;
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            j = even(j);
+                            count++;
+                        }
+                        else
+                        {
+                            j = uneven(j);
+                            count++;
+                        }
+                    }
+                    
+                }
+                count++;
+                collatzList[i, 0] = i;
+                collatzList[i, 1] = count;
+                if (count < longestLength) continue;
+                longestNum = i;
+                longestLength = count;
+            }
+            return collatzList[longestNum, 1];
+        }
+    }
+}
---
+++ Tests/001-100/014/014.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _014_Longest_Collatz_sequence;
+
+namespace Tests._001_100
+{
+    [TestClass]
+    public class _014
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = Class1.LongestCollatz(13);
+            Console.WriteLine(result);
+        }
+    }
+}
