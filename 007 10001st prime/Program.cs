fac71213cbb9cecbf7135ce469318907701fe754
emilrl <emil1708@gmail.com>
2015-11-04 13:53:56 +0100

    Change folder names

---
+++ 007 10001st prime/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem007
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine(Prime(100001));
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedMilliseconds + " ms");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Description of function.
+        /// </summary>
+        /// <param name="n">Number to check</param>
+        /// <returns></returns>
+        public static bool NumberIsPrime(long n)
+        {
+            var sqrt = Convert.ToInt64(Math.Sqrt(n));
+            var isPrime = true;
+            for (long i = 2; i <= sqrt; i++)
+            {
+                if (n%i != 0) continue;
+                isPrime = false;
+                break;
+            }
+            return isPrime;
+        }
+
+        public static long Prime(int n)
+        {
+            var count = 1;
+            long i = 1;
+            while (count < n)
+            {
+                i += 2;
+                if (NumberIsPrime(i))
+                {
+                    count++;
+                }
+
+            }
+            return i;
+        }
+    }
+}
---
+++ 009 Special Pythagorean triplet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem009
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int PytTripProd(int sum)
+        {
+            int c;
+            for (c = 3; c < (sum/2); c++)
+            {
+                int a;
+                for (a = 1; a < ((sum/2) - 2); a++)
+                {
+                    var b = sum - (a + c);
+                    if (a + b > (sum/2) && a + b + c == 1000 && Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                    {
+                        return a*b*c;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        //public static List<double> MakePowList(int c)
+        //{
+        //    var maxPow = c;
+        //    var powList = new List<double>();
+        //    for (double i = 1; i <= Math.Sqrt(maxPow); i++)
+        //    {
+        //        powList.Add(Math.Pow(i, 2));
+        //    }
+        //    return powList;
+        //}
+    }
+}
